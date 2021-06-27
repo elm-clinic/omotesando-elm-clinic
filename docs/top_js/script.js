9c996c27bc25bf691307d6d4c865aaba61f6266e
elm-clinic <sho_utsumi@elm-clinic.jp>
2021-06-27 11:15:20 +0900

    iframe, モーダル, トップのスライドショーを追加

--- docs/top_js/script.js
+++ docs/top_js/script.js
@@ -128,3 +128,34 @@ $('.slider').slick({
 	}
 ]
 });
+
+
+
+
+//動画のモーダル
+$(".video-open").modaal({
+	type: 'video',
+	overlay_close:true,//モーダル背景クリック時に閉じるか
+	background: '#28BFE7', // 背景色
+	overlay_opacity:0.8, // 透過具合
+	before_open:function(){// モーダルが開く前に行う動作
+		$('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
+	},
+	after_close:function(){// モーダルが閉じた後に行う動作
+		$('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
+	}
+});
+
+//iframeのモーダル
+$(".iframe-open").modaal({
+	type:'iframe',
+	width: 800,//iframe横幅
+	height:800,//iframe高さ
+	overlay_close:true,//モーダル背景クリック時に閉じるか
+before_open:function(){// モーダルが開く前に行う動作
+	$('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
+},
+after_close:function(){// モーダルが閉じた後に行う動作
+	$('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
+}
+});

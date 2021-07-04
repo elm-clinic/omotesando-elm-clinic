1b52ad2cfeeddc02c885111e394c902cd41a42d8
elm-clinic <sho_utsumi@elm-clinic.jp>
2021-07-04 14:28:53 +0900

    スワイプの設定

--- docs/top_js/script.js
+++ docs/top_js/script.js
@@ -106,6 +106,7 @@ $(window).on('load',function(){
 $('.slider').slick({
 	autoplay: true,//自動的に動き出すか。初期値はfalse。
 	infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+	swipe: true,
 	slidesToShow: 3,//スライドを画面に3枚見せる
 	slidesToScroll: 3,//1回のスクロールで3枚の写真を移動して見せる
 	prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更

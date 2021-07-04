79bca1e8a97729ba4c633b733c75bb30023dc5d2
elm-clinic <sho_utsumi@elm-clinic.jp>
2021-07-04 13:52:07 +0900

    院長紹介の追加とスクロールエリアの変更

--- docs/top_js/script.js
+++ docs/top_js/script.js
@@ -1,7 +1,7 @@
 //スクロールした際の動きを関数でまとめる
 function PageTopCheck(){
   var winScrollTop = $(this).scrollTop();
-  var secondTop =  $("#area-2").offset().top - 150; //#area-2の上から150pxの位置まで来たら
+  var secondTop =  $("#area-1").offset().top - 150; //#area-1の上から150pxの位置まで来たら
   if(winScrollTop >= secondTop){
   $('.js-scroll').removeClass('scroll-view');//.js-scrollからscroll-viewというクラス名を除去
   $('.js-pagetop').addClass('scroll-view');//.js-pagetopにscroll-viewというクラス名を付与
@@ -15,9 +15,9 @@ function PageTopCheck(){
 //クリックした際の動き
 $('.scroll-top a').click(function () {
 var elmHash = $(this).attr('href'); //hrefの内容を取得
-if (elmHash == "#area-2") {//もし、リンク先のhref の後が#area-2の場合
+if (elmHash == "#area-1") {//もし、リンク先のhref の後が#area-1の場合
   var pos = $(elmHash).offset().top;
-  $('body,html').animate({scrollTop: pos}, pos); //#area-2にスクロール
+  $('body,html').animate({scrollTop: pos}, pos); //#area-1にスクロール
 }else{
   $('body,html').animate({scrollTop: 0}, 500); //それ以外はトップへスクロール。数字が大きくなるほどゆっくりスクロール
 }
